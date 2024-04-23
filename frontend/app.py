2a76737ada4682659906e6e096b0da41787f4192
Deepanshuisjod <deepanshudb1@gmail.com>
2024-04-23 23:08:12 +0530

    modify app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+class User :
+    def __init__(self,username,password,confpass):
+        self.username = username
+        self.password = password
+        self.confpass = confpass
+
+
 
 @app.route("/")
 def welcome():
     return "<p>Welcome</p>"
+
+@app.route("/register")
+def register():
+    return render_template('auth.html')
